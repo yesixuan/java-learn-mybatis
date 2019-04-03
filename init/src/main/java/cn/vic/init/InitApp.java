de7667a1ceba2e21d77be484722622133cb2c63d
yesixuan <18373209286@163.com>
2019-04-03 10:38:35 +0800

    feat: 删除一些无用的代码

--- init/src/main/java/cn/vic/init/InitApp.java
+++ init/src/main/java/cn/vic/init/InitApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(value = "cn.vic.init.mapper", nameGenerator = MapperNameGenerator.class)
+@MapperScan(value = "cn.vic.init.mapper")
 public class InitApp {
 
     public static void main(String[] args) {
